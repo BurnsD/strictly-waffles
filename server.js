9897d335ef655c9fada77e58da8bc181780d0215
BurnsD <Dustinburnsactech@gmail.com>
2022-02-05 20:21:12 -0600

    server setup/get request

--- server.js
+++ server.js
@@ -4,3 +4,26 @@ const req = require('express/lib/request');
 const path =require('path');
 const fs = require('fs');
 const util = require('util');
+
+// File Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware for static page
+app.use(express.static('../public'));
+
+// GET request
+app.get('/api/notes', (req, res) => {
+    readFileAsync('../db/db.json', "utf-8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+// POST request
